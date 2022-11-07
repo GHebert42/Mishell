64bb6de29058a583db4830580774009fc4629f40
GHebert42 <hebertgermain.qc@gmail.com>
2022-11-07 06:17:53 -0500

    base_maker

---
+++ srcs/main.c
@@ -0,0 +1,20 @@
+
+/*
+
+
+    #include <stdio.h>
+       #include <readline/readline.h>
+       #include <readline/history.h>
+
+       char *
+       readline (const char *prompt);
+       
+       
+*/
+
+#include "../incs/minishell.h"
+
+int main(int ac, char **av, char **envp)
+{
+    
+}
